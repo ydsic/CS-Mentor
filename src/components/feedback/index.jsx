daf6018e1f0b25eae3cf91e05e5da1add39acf72
양아름 <yangareum0130@gmail.com>
2025-05-15 20:43:31 +0900

    design : 피드백섹션  스크롤 추가

--- src/components/feedback/index.jsx
+++ src/components/feedback/index.jsx
@@ -2,9 +2,11 @@ import ReactMarkdown from "react-markdown";
 
 export default function Feedback({ feedback }) {
   return (
-    <div className="mt-4 w-full max-w-5xl p-4 bg-white border-l-4 border-[#1e40af]">
-      <strong>피드백:</strong>
-      <div className="mt-2 prose prose-blue max-w-none">
+    <div className="w-full h-96 max-w-5xl p-4">
+      <strong className="inline-block w-fit px-4 bg-[#1e40af] text-white">
+        피드백 :
+      </strong>
+      <div className="my-4 p-4 max-w-none bg-white border-l-4 border-[#1e40af]">
         <ReactMarkdown>{feedback}</ReactMarkdown>
       </div>
     </div>

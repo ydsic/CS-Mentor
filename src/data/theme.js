024b73910b7e24cad18bf5b8839c39eff07d1844
ydsic <ydsic99@gmail.com>
2025-05-16 15:51:03 +0900

    fix: 색상 수정

--- src/data/theme.js
+++ src/data/theme.js
@@ -1,6 +1,6 @@
 export const THEMES = [
   { name: "white", classes: "bg-white text-gray-800", color: "#FFFFFF" },
-  { name: "black", classes: "bg-black text-white", color: "#000000" },
+  { name: "black", classes: "bg-[#17171B] text-white", color: "#000000" },
   { name: "skyblue", classes: "bg-sky-200 text-gray-800", color: "#38BDF8" },
   { name: "rose", classes: "bg-rose-200 text-gray-800", color: "#F43F5E" },
   { name: "lime", classes: "bg-lime-200 text-gray-800", color: "#A3E635" },

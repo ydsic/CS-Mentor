1b39b74c75d9a9be84246e2af5488cc8e6ecf9f2
ydsic <ydsic99@gmail.com>
2025-05-23 13:17:58 +0900

    fix: vite.config.js 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/may_study_project/",
   plugins: [react(), tailwindcss()],
 });

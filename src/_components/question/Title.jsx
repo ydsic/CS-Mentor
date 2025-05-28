42fe63aa3fb9bf95abed9dab66e29dc2ad8a8a05
ydsic <ydsic99@gmail.com>
2025-05-28 20:43:50 +0900

    style: Title 스타일 수정

--- src/_components/question/Title.jsx
+++ src/_components/question/Title.jsx
@@ -1,3 +1,3 @@
 export default function Title() {
-  return <h1 className="text-4xl text-[#1e40af] font-bold">CS Mentor</h1>;
+  return <h1 className="text-4xl text-[#1e40af] font-bold mb-1">CS Mentor</h1>;
 }

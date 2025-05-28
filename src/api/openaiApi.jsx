d532e5a158d7313a2c317ae6f6565f6036b5f1ad
ydsic <ydsic99@gmail.com>
2025-05-28 17:26:02 +0900

    refactor: API 파일 분리

--- src/api/openaiApi.jsx
+++ src/api/openaiApi.jsx
@@ -1,21 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vlowdzoigoyaudsydqam.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsb3dkem9pZ295YXVkc3lkcWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MDg3NTUsImV4cCI6MjA1ODk4NDc1NX0.7ltcwu8G4_awXU5SFkAXRGnSeThjTTqAOVUm1bjtmnU";
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { getOpenAIApiKey } from "./supabaseApi";
+
 export async function OpenAIApi(question, input) {
-  const { data, error } = await supabase
-    .from("openaikey")
-    .select("openai")
-    .limit(1)
-    .single();
-
-  if (error) {
-    console.error("Supabase Error:", error);
-    throw new Error("OpenAI Key 조회 실패");
-  }
-
-  const apiKey = data.openai;
+  const apiKey = await getOpenAIApiKey();
 
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
